bd639f20086894461bdd8e5bcff7b47ee8b21336
axellelarios <al7@gotmail.fr>
2024-07-24 19:27:01 +0200

    project

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 const express = require("express"); // import du package express
-const cors = require("cors")
-const axios = require("axios");
-
-//le module cors permet d'autoriser ou non les demandes provenant de l'extérieur.
 const app = express(); // création du serveur
-app.use(cors());
 
 // Variables DOTENV
 // Permet d'activer les variables d'environnement qui se trouvent dans le fichier `.env`  
 require('dotenv').config();
 
+const axios = require("axios");
+
+const cors = require("cors")
+//le module cors permet d'autoriser ou non les demandes provenant de l'extérieur.
+app.use(cors());
+
 // Import clé 
 const apiKey = process.env.API_KEY;
 
-app.get("/", (req, res) => { // route en GET dont le chemin est /
+
+app.get("/", async (req, res) => { // route en GET dont le chemin est /
   try {
-    const data = axios.get(`https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=QiNfGGKViu0A9iDb`);
-    console.log(data)
-    return res.status(200).json(data)
+    const data = await axios.get(`https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${apiKey}`);
+    res.status(200).json(data)
   } catch (err) {
       res.status(500).json({ message: 'Server error' })
   }

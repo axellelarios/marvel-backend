ec23d7dc30b142b6aa5c29e49df09c020f7aa334
axellelarios <al7@gotmail.fr>
2024-07-24 18:06:23 +0200

    push

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const express = require("express"); // import du package express
 const app = express(); // création du serveur
+
+//le module cors permet d'autoriser ou non les demandes provenant de l'extérieur.
+const cors = require("cors")
 app.use(cors());
+
+// Variables DOTENV
+// Permet d'activer les variables d'environnement qui se trouvent dans le fichier `.env`  
+require('dotenv').config();
 
 // import axios
 const axios = require("axios");
 
-const apiKey = "QiNfGGKViu0A9iDb"
-
 app.get("/", (req, res) => { // route en GET dont le chemion est /
   axios
-  .get("https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=apiKey")
+  .get(`https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${API_KEY}`)
   .then((response) => {
     console.log(response.data); // Affichera la réponse du serveur
+    res.json({message: "It's live bitches!!"});
   })
   .catch(error => {
     console.log(error.message); // Affichera d'éventuelles erreurs, notamment en cas de problème de connexion Internet.

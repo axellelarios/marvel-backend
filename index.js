824034f34f8d14ef1c5b62b2771097048cabfa9e
axellelarios <al7@gotmail.fr>
2024-07-24 19:39:21 +0200

    hey

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const cors = require("cors")
 //le module cors permet d'autoriser ou non les demandes provenant de l'extérieur.
 app.use(cors());
 
+// Utilisation des parametre body
+app.use(express.json())
+
 // Import clé 
 const apiKey = process.env.API_KEY;
 
@@ -18,7 +21,7 @@ const apiKey = process.env.API_KEY;
 app.get("/", async (req, res) => { // route en GET dont le chemin est /
   try {
     const data = await axios.get(`https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${apiKey}`);
-    return res.status(200).send(data)
+    return res.status(200).json(data)
   } catch (err) {
        return res.status(401).send(err.message);
   }
